39fe9870db46fcc0cce7289740efaff5f485293c
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-16 20:05:12 +0530

    solve cchef - mario

---
+++ codechef/Mario_and_Transformation.cpp
@@ -0,0 +1,40 @@
+/*Om Namo Narayana~*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 3 == 0)
+        cout << "NORMAL\n";
+    else if (n % 3 == 1)
+        cout << "HUGE\n";
+    else
+        cout << "SMALL\n";
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
