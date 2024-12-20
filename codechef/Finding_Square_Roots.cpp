7f6dab2efd1f4be4f67b88aa8db83d41f37ead80
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-21 02:51:00 +0530

    Add solution for CodeChef problem Finding Square Roots

---
+++ codechef/Finding_Square_Roots.cpp
@@ -0,0 +1,35 @@
+/*Om Namo Narayana~*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << (int)sqrt(n) << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
