2395b1ae52463defbd7e8d57d24369a8edcfd92f
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-21 03:28:08 +0530

    Add solution for CodeChef problem Blackjack

---
+++ codechef/Blackjack.cpp
@@ -0,0 +1,41 @@
+/*Om Namo Narayana~*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int x,y;
+    cin>>x>>y;
+    int sum = x+y;
+    if(21-sum<=10&&sum>0){
+        cout<<21-sum<<'\n';
+    }
+    else{
+        cout<<-1<<'\n';
+    }
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
