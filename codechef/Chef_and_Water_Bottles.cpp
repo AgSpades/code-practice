634c21ba64869b911d339efe743c7ca0255b4368
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-20 18:33:53 +0530

    Add solution for CodeChef problem Chef and Water Bottles

---
+++ codechef/Chef_and_Water_Bottles.cpp
@@ -0,0 +1,37 @@
+/*Om Namo Narayana~*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n, x, k;
+    cin >> n >> x >> k;
+    int q=k/x;
+    if(q<n) cout<<q<<endl;
+    else cout<<n<<endl;
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
