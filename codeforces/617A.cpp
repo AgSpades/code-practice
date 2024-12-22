40f9ea781743b2c17c14ad434988b4ea16596682
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-22 19:44:52 +0530

    Add solution for Codeforces problem 617A

---
+++ codeforces/617A.cpp
@@ -0,0 +1,30 @@
+/*Om Namo Narayana~*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int x;
+    cin >> x;
+    x<=5? cout << 1 :(x%5==0?cout<<x/5 :cout << ((x)/5)+1);
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
