301b6a5ea88ffa0d7d5c1c209ca23a5c925512e7
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-22 22:48:22 +0530

    Add solution for CodeChef problem It is My Serve

---
+++ codechef/It_is_My_Serve.cpp
@@ -0,0 +1,42 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    int t=a+b;
+    if(t%4==0||t%4==1)cout<<"Alice\n";
+    else cout<<"Bob\n";
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
