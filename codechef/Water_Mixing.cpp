ff51bd2576c734e3b46d9cef686f07ac7e11235f
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-23 20:13:52 +0530

    Add solution for CodeChef problem Water Mixing

---
+++ codechef/Water_Mixing.cpp
@@ -0,0 +1,48 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+    int target = b - a;
+    bool flag = false;
+    if(target<=0&&y>=abs(target)){
+        flag = true;
+    }
+    if(target>=0&&x>=target){
+        flag = true;
+    }
+    flag?cout<<"YES\n":cout<<"NO\n";
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
