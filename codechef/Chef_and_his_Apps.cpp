87443c012a11c3a3cb71880499df65760d94a31b
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-24 19:19:37 +0530

    Add solution for CodeChef problem Chef and his Apps

---
+++ codechef/Chef_and_his_Apps.cpp
@@ -0,0 +1,51 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int s, x, y, z;
+    cin >> s >> x >> y >> z;
+    if (s - x - y >= z)
+    {
+        cout << 0 << endl;
+    }
+    else if (s - x >= z || s - y >= z)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 2 << endl;
+    }
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
