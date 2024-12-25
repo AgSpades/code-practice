4b952cf51b97a362d2bf64a7bf4be7f0987e747c
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-25 20:00:32 +0530

    Add solution for CodeChef problem Chef Eren

---
+++ codechef/Chef_Eren.cpp
@@ -0,0 +1,49 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    int sum = 0;
+    FOR(i, 1, n + 1)
+    {
+        if (i & 1)
+            sum += b;
+        else
+            sum += a;
+    }
+
+    cout << sum << "\n";
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
