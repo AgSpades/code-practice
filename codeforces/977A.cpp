934c5f4ebad01dc553e0c543ad5f058241c3f3e7
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-25 21:32:41 +0530

    Add solution for Codeforces problem 977A

---
+++ codeforces/977A.cpp
@@ -0,0 +1,42 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    while (k--)
+    {
+        if (n % 10 == 0)
+            n /= 10;
+        else
+            n--;
+    }
+    cout << n << '\n';
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
