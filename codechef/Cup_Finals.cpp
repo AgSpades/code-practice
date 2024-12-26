4fb3fb7c360e7097d3f1ad269eb08f1cd5e04336
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-26 22:44:40 +0530

    Add solution for CodeChef problem Cup Finals

---
+++ codechef/Cup_Finals.cpp
@@ -0,0 +1,47 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int x, y, d;
+    cin >> x >> y >> d;
+    if (abs(x - y) <= d)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
