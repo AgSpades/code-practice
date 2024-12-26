834531da3b99053b5d1b89004b646a2be6179a4f
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-26 19:39:42 +0530

    Add solution for CodeChef problem Minimum Number of Coins

---
+++ codechef/Minimum_number_of_coins.cpp
@@ -0,0 +1,49 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int x;
+    cin >> x;
+    if (x % 10 == 0)
+    {
+        cout << x / 10 << '\n';
+    }
+    else if (x % 5 == 0)
+    {
+        cout << (x / 10) + 1 << '\n';
+    }
+    else
+        cout << -1 << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
