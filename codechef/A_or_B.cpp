b9feceae16644cb22e8897914bd1a7f69f83ae4b
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-28 09:30:40 +0530

    Add solution for CodeChef problem A or B

---
+++ codechef/A_or_B.cpp
@@ -0,0 +1,41 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int a,b,ta,tb;
+    cin>>a>>b;
+    ta= 500-a*2+1000-(a+b)*4;
+    tb= 500-(a+b)*2+1000-(b)*4;
+    ta>tb?cout<<ta<<'\n':cout<<tb<<'\n';
+}
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
