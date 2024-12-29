b65cc45f5d2c179698e489924cc56b3d5607d15c
AgSpades <86034982+AgSpades@users.noreply.github.com>
2024-12-29 10:22:18 +0530

    Add solution for CodeChef problem Chef Fantasy 11

---
+++ codechef/Chef_Fantasy_11.cpp
@@ -0,0 +1,40 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << n * (n - 1) << "\n";
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
