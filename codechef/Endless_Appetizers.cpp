95d465983b363aad4c9a66b3dfd1adbbb0db767e
AgSpades <86034982+AgSpades@users.noreply.github.com>
2025-01-01 21:03:50 +0530

    Add solution for CodeChef problem Endless Appetizers

---
+++ codechef/Endless_Appetizers.cpp
@@ -0,0 +1,49 @@
+/*
+Om Namo Narayana~
+author: agspades
+year of code: 2024
+template version: 0.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int x, y, r;
+    cin >> x >> y >> r;
+    int extra=r/30;
+    int tot =extra+x;
+    if(tot%y==0)
+    {
+        cout<<tot/y<<endl;
+    }
+    else
+    {
+        cout<<tot/y+1<<endl;
+    }
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
