aa6ec456f2f121eaaec2a64d8cd31b4ebfb7aca5
Saumyajit <86034982+AgSpades@users.noreply.github.com>
2025-02-01 20:43:43 +0530

    Add solution for Codeforces 486A

---
+++ codeforces/486A.cpp
@@ -0,0 +1,40 @@
+/*
+Jai Bajrangbali!
+author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll sum;
+    if(n&1) sum= (n+1)/2 *-1;
+    else sum = n/2;
+
+    cout<<sum;
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
