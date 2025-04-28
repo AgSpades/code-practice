b9477a3284b50be7d4c531f7d0ae8db832a2a05d
Saumyajit P <86034982+AgSpades@users.noreply.github.com>
2025-04-29 00:02:48 +0530

    Add solution for Kattis problem Carrots

---
+++ kattis/carrots.cpp
@@ -0,0 +1,36 @@
+/*
+Jai Bajrangbali!
+author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n, p;
+    scanf("%d %d", &n, &p);
+    printf("%d", p);
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
