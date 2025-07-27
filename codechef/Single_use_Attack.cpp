5fd12b777c56e50aa3aad63e8d2124c969ce1a02
Saumyajit P. <psaumyajit@outlook.com>
2025-07-28 00:29:30 +0530

    Add solution for CodeChef problem Single Use Attack

---
+++ codechef/Single_use_Attack.cpp
@@ -0,0 +1,53 @@
+/*
+Jai Bajrangbali!
+author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int h, x, y;
+    cin >> h >> x >> y;
+    int moves = 0;
+    int bh = h - y;
+    moves++;
+    if (bh > 0)
+    {
+        while (bh != 0 && bh > 0)
+        {
+            bh -= x;
+            moves++;
+        }
+    }
+
+    cout << moves << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
