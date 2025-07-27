04fb234798eeb3c0582aabe7ea757b2f5e371e12
Saumyajit P. <psaumyajit@outlook.com>
2025-07-28 00:48:55 +0530

    Add solution for CodeChef problem Minimum Number of Flips

---
+++ codechef/Minimum_number_of_Flips.cpp
@@ -0,0 +1,52 @@
+/*
+Jai Bajrangbali!
+author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    if (n & 1)
+        cout << -1 << '\n';
+    else
+    {
+        int np = 0, nn = 0;
+        for (int num : arr)
+            num > 0 ? np++ : nn++;
+        np > nn ? cout << n / 2 - nn << '\n' : cout << n / 2 - np << '\n';
+    }
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
