ebfd7f1ab8c45b90883773d824d86c9a8d283eb9
Saumyajit P. <psaumyajit@outlook.com>
2025-07-28 00:01:58 +0530

    Add solution for CodeChef problem Mutated Minions

---
+++ codechef/Mutated_Minions.cpp
@@ -0,0 +1,50 @@
+/*
+Jai Bajrangbali!
+author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n, k;
+    int c = 0;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        arr[i] += k;
+        if (arr[i] % 7 == 0)
+            c++;
+    }
+    cout << c << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
