a213460fce70e81b7ab044ec030e06b995547f30
Saumyajit P. <psaumyajit@outlook.com>
2025-09-01 20:23:37 +0530

    Add solution for Codeforces problem 118A - Vowel and Consonant

---
+++ codeforces/118A.cpp
@@ -0,0 +1,56 @@
+/*
+Jai Bajrangbali!
+author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+bool isVowel(char &c)
+{
+    c = tolower(c);
+
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y';
+}
+
+inline void solve()
+{
+    string s;
+    cin >> s;
+    vector<char> consonants;
+    for (char c : s)
+    {
+        if (!isVowel(c))
+        {
+            consonants.pb(tolower(c));
+        }
+    }
+    for (int i = 0; i < consonants.size(); i++)
+    {
+        cout << "." << consonants[i];
+    }
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
