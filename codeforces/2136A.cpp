a04f9579ed6305ff2a3a3c850a10983194f77904
Saumyajit P. <psaumyajit@outlook.com>
2025-09-08 19:29:34 +0530

    Add solution for Codeforces problem 2136A

---
+++ codeforces/2136A.cpp
@@ -0,0 +1,48 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    c -= a, d -= b;
+    if (a > b)
+        swap(a, b);
+    if (c > d)
+        swap(c, d);
+    puts((a + 1 << 1) >= b && (c + 1 << 1) >= d ? "YES" : "NO");
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
