272a19c08c4a8f6ec30d0517369d75a64bbc2d31
Saumyajit P. <psaumyajit@outlook.com>
2025-09-11 19:05:42 +0530

    Add solution for Codechef problem Easy Pronunciation

---
+++ codechef/Easy_Pronunciation.cpp
@@ -0,0 +1,57 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+bool isVowel(char c)
+{
+    c = tolower(c);
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+inline void solve()
+{
+    int n;
+    string word;
+    cin >> n >> word;
+    int count = 0;
+    for (char c : word)
+    {
+        count = isVowel(c) ? 0 : count + 1;
+        if (count >= 4) break;
+    }
+
+    puts(count >= 4 ? "NO" : "YES");
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
