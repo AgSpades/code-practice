d2fde3b85d87bf7bb1fd71c9d13fd44a8e521023
Saumyajit P. <psaumyajit@outlook.com>
2025-09-13 12:23:47 +0530

    Add solution for Kattis problem Basketball One-on-One

---
+++ kattis/basketballoneonone.cpp
@@ -0,0 +1,38 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2025
+template version: 0.1.1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    string s;
+    cin >> s;
+    cout << s[s.size() - 2];
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
