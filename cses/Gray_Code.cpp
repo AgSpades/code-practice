8b6adfb50118966bc9c4df4bc2f0709f13714e45
Saumyajit P. <psaumyajit@outlook.com>
2025-10-09 16:01:16 +0530

    Add solution for CSES problem Gray Code

---
+++ cses/Gray_Code.cpp
@@ -0,0 +1,69 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2025
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Binary Exponentian Function
+ll binpow(ll a, ll b, ll mod = MOD)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+inline void solve()
+{
+    int n;
+    cin >> n;
+    vector<string> bits = {"0", "1"};
+    for (int i = 2; i <= n; i++)
+    {
+        vector<string> new_bits;
+        for (const string &s : bits)
+        {
+            new_bits.push_back("0" + s);
+        }
+        for (int j = bits.size() - 1; j >= 0; j--)
+        {
+            new_bits.push_back("1" + bits[j]);
+        }
+        bits = new_bits;
+    }
+    for (const string &a : bits)
+    {
+        cout << a << '\n';
+    }
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
