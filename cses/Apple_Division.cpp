cc9fea870510a715e241da13580eefaa442b4c10
Saumyajit P. <mail@saumyajit.dev>
2026-01-05 23:31:34 +0530

    Add solution for CSES problem Apple Division

---
+++ cses/Apple_Division.cpp
@@ -0,0 +1,70 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Binary Exponentian Function
+ll binpow(ll a, ll b, ll mod = MOD)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll minSolve(int index, ll *arr, ll sum1, ll sum2, int n)
+{
+    if (index == n)
+    {
+        return abs(sum1 - sum2);
+    }
+
+    ll select = minSolve(index + 1, arr, sum1 + arr[index], sum2, n);
+    ll notSelect = minSolve(index + 1, arr, sum1, sum2 + arr[index], n);
+
+    return min(select, notSelect);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    ll arr[n];
+    FOR(i, 0, n)
+    {
+        cin >> arr[i];
+    }
+    cout << minSolve(0, arr, 0, 0, n) << '\n';
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
