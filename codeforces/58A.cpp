67bf07a420581440311de8cd9d7c1d684bbc6a15
Saumyajit P. <mail@saumyajit.dev>
2026-01-06 15:23:22 +0530

    Add solution for Codeforces problem 58A - "Hello"

---
+++ codeforces/58A.cpp
@@ -0,0 +1,63 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// Binary Exponentian Function
+ll binpow(ll a, ll b, ll mod = MOD)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve()
+{
+    string s;
+    cin>>s;
+    string target="hello";
+    int pos =0;
+    for(char c:s){
+        if(c==target[pos]){
+            pos++;
+            if(pos==5){
+                cout<<"YES\n";
+                return;
+            }
+        }
+    }
+    cout<<"NO\n";
+}
+
+int main()
+{
+    fastio;
+    solve();
+    return 0;
+}
