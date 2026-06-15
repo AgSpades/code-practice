a65da60cc91fe01a58179385430594927169aa4d
Saumyajit P. <mail@saumyajit.dev>
2026-06-15 07:14:05 +0000

    Add solution for LeetCode problem 1209 - Remove All Adjacent Duplicates in String II

---
+++ leetcode/1209.remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=1209 lang=cpp
+ *
+ * [1209] Remove All Adjacent Duplicates in String II
+ *
+ * https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/description/
+ *
+ * algorithms
+ * Medium (61.40%)
+ * Likes:    6117
+ * Dislikes: 123
+ * Total Accepted:    429.3K
+ * Total Submissions: 699.1K
+ * Testcase Example:  '"abcd"\n2'
+ *
+ * You are given a string s and an integer k, a k duplicate removal consists of
+ * choosing k adjacent and equal letters from s and removing them, causing the
+ * left and the right side of the deleted substring to concatenate together.
+ *
+ * We repeatedly make k duplicate removals on s until we no longer can.
+ *
+ * Return the final string after all such duplicate removals have been made. It
+ * is guaranteed that the answer is unique.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "abcd", k = 2
+ * Output: "abcd"
+ * Explanation: There's nothing to delete.
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "deeedbbcccbdaa", k = 3
+ * Output: "aa"
+ * Explanation:
+ * First delete "eee" and "ccc", get "ddbbbdaa"
+ * Then delete "bbb", get "dddaa"
+ * Finally delete "ddd", get "aa"
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "pbbcggttciiippooaais", k = 2
+ * Output: "ps"
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 10^5
+ * 2 <= k <= 10^4
+ * s only contains lowercase English letters.
+ *
+ *
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    string removeDuplicates(string s, int k)
+    {
+        string foo;
+        for (char c : s)
+        {
+            foo.push_back(c);
+            if (foo.length() >= k)
+            {
+                //check last k elements
+                int p = foo.size()-k;
+                bool l = true;
+                for(int i = foo.size()-1;i>=p;i--){
+                    if(foo[i]!=c){
+                        l=false;
+                        break;
+                    }
+                }
+                if(l){
+                    for(int i=0;i<k;i++){
+                        foo.pop_back();
+                    }
+                }
+                
+            }
+        }
+
+        return foo;
+    }
+};
+// @lc code=end
