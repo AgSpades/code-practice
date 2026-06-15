89404c9bb136f06cb6cbce1ea161cf5c12fd3eb8
Saumyajit P. <mail@saumyajit.dev>
2026-06-15 09:09:03 +0000

    Add solution for LeetCode problem 735 - Asteroid Collision

---
+++ leetcode/735.asteroid-collision.cpp
@@ -0,0 +1,125 @@
+/*
+ * @lc app=leetcode id=735 lang=cpp
+ *
+ * [735] Asteroid Collision
+ *
+ * https://leetcode.com/problems/asteroid-collision/description/
+ *
+ * algorithms
+ * Medium (48.09%)
+ * Likes:    9467
+ * Dislikes: 1307
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[5,10,-5]'
+ *
+ * We are given an array asteroids of integers representing asteroids in a row.
+ * The indices of the asteroid in the array represent their relative position
+ * in space.
+ *
+ * For each asteroid, the absolute value represents its size, and the sign
+ * represents its direction (positive meaning right, negative meaning left).
+ * Each asteroid moves at the same speed.
+ *
+ * Find out the state of the asteroids after all collisions. If two asteroids
+ * meet, the smaller one will explode. If both are the same size, both will
+ * explode. Two asteroids moving in the same direction will never meet.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: asteroids = [5,10,-5]
+ * Output: [5,10]
+ * Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never
+ * collide.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: asteroids = [8,-8]
+ * Output: []
+ * Explanation: The 8 and -8 collide exploding each other.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: asteroids = [10,2,-5]
+ * Output: [10]
+ * Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide
+ * resulting in 10.
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: asteroids = [3,5,-6,2,-1,4]​​​​​​​
+ * Output: [-6,2,4]
+ * Explanation: The asteroid -6 makes the asteroid 3 and 5 explode, and then
+ * continues going left. On the other side, the asteroid 2 makes the asteroid
+ * -1 explode and then continues going right, without reaching asteroid 4.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 2 <= asteroids.length <= 10^4
+ * -1000 <= asteroids[i] <= 1000
+ * asteroids[i] != 0
+ *
+ *
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> asteroidCollision(vector<int> &asteroids)
+    {
+        stack<int> fs;
+
+        for (int asteroid : asteroids)
+        {
+            bool boom = false;
+            while (!fs.empty() && fs.top() > 0 && asteroid < 0) // explodes only when top is moving right and incoming is coming left
+            {
+                if (fs.top() < abs(asteroid)) // incoming is big
+                {
+                    fs.pop();
+                    continue;
+                }
+                else if (fs.top() == abs(asteroid)) // same both explodes
+                {
+                    fs.pop();
+                    boom = true;
+                    break;
+                }
+                else
+                { // incoming small it explodes
+                    boom = true;
+                    break;
+                }
+            }
+            if (!boom)
+            {
+                fs.push(asteroid);
+            }
+        }
+
+        vector<int> res;
+        while (!fs.empty())
+        {
+            res.push_back(fs.top());
+            fs.pop();
+        }
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
+// @lc code=end
