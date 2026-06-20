8ac5cb4ddb914ca56cd3325dd2f2f8ad0e23396c
Saumyajit P. <mail@saumyajit.dev>
2026-06-20 06:58:53 +0000

    Add solution for Codeforces problem 1903A - Halloumi Boxes

---
+++ codeforces/1903A.cpp
@@ -0,0 +1,54 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vi arr(n);
+    FOR(i, 0, n)
+    {
+        cin >> arr[i];
+    }
+    bool can = false;
+    if (k > 1 || is_sorted(arr.begin(), arr.end()))
+    {
+        can = true;
+    }
+
+    can ? cout << "YES\n" : cout << "NO\n";
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
