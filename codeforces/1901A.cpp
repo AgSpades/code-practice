329e760eba0826188462aa992740270a78318e75
Saumyajit P. <mail@saumyajit.dev>
2026-06-23 15:24:30 +0530

    Add solution for Codeforces problem 1901A - Line Trip

---
+++ codeforces/1901A.cpp
@@ -0,0 +1,55 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vi arr(n);
+    FOR(i, 0, n)
+    cin >> arr[i];
+
+    int d = arr[0], md = 0;
+
+    FOR(i, 1, n)
+    {
+        d = max(d, arr[i] - arr[i - 1]);
+    }
+    d = max(d, 2 * (x - arr[n - 1]));
+
+    cout << d << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
