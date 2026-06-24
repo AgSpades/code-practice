af772af293b6584d2d8f75be99d294852fa38923
Saumyajit P. <mail@saumyajit.dev>
2026-06-24 12:47:21 +0530

    Add solution for Codeforces problem 1899A - Game with Integers

---
+++ codeforces/1899A.cpp
@@ -0,0 +1,43 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n;
+    scanf("%d", &n);
+    printf(n % 3 ? "First\n" : "Second\n");
+}
+
+int main()
+{
+    fastio;
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
