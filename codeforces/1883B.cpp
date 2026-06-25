59268cdfd03217b44acc6c12796228de4676b835
Saumyajit P. <mail@saumyajit.dev>
2026-06-26 02:15:32 +0530

    Add solution for Codeforces problem 1883B - Chemistry

---
+++ codeforces/1883B.cpp
@@ -0,0 +1,52 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    char cset[26] = {0};
+    for (char c : s)
+        cset[c - 'a'] ^= 1;  // track odd or even for each character
+    int odds = 0;
+    for (int c : cset)
+        odds += c;
+    puts(
+        k < odds - 1 ? "NO" : "YES");
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
