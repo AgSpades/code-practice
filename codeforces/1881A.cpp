6da8fc679c15d37b23b1e078600ce8bc33d3ea75
Saumyajit P. <mail@saumyajit.dev>
2026-06-29 23:06:05 +0530

    Add solution for Codeforces problem 1881A - SDon't Try to Count

---
+++ codeforces/1881A.cpp
@@ -0,0 +1,54 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    string x, s;
+    cin >> x >> s;
+    FOR(i, 0, 6)
+    {
+        if (x.find(s) != string::npos)
+        {
+            cout << i << '\n';
+            return;
+        }
+        x += x;
+    }
+    cout << -1 << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
