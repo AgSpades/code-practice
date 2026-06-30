e3173ad7819de5466d1333f3577652de4bc5e928
Saumyajit P. <mail@saumyajit.dev>
2026-06-30 15:06:23 +0530

    Add solution for Codeforces problem 900A - How Much Does Daytona Cost?

---
+++ codeforces/900A.cpp
@@ -0,0 +1,52 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+
+inline void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    bool f = false;
+    FOR(i, 0, n)
+    {
+        int t;
+        cin >> t;
+        if (t == k)
+            f = true;
+    }
+    puts(f ? "YES" : "NO");
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
