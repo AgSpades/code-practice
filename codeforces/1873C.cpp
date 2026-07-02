a698d65e34e9a4f2e72212c220cba7c3fc489782
Saumyajit P. <mail@saumyajit.dev>
2026-07-03 01:16:28 +0530

    Add solution for Codeforces problem 1873C - Target Practice

---
+++ codeforces/1873C.cpp
@@ -0,0 +1,55 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int res = 0;
+    FOR(i, 0, 10)
+    {
+        FOR(j, 0, 10)
+        {
+            char c;
+            cin >> c;
+            if (c == 'X')
+            {
+                // minimum distance from the edge
+                res += min({i + 1, 10 - i, j + 1, 10 - j});
+            }
+        }
+    }
+    cout << res << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
