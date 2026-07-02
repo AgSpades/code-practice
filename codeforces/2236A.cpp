df5fadbcd2d7283f8b3691d1c697df429259ccc6
Saumyajit P. <mail@saumyajit.dev>
2026-07-02 22:37:00 +0530

    Add solutions for Codeforces problems 2236A, 2236B, and 2241A

---
+++ codeforces/2236A.cpp
@@ -0,0 +1,49 @@
+/*
+Jai Bajrangbali!
+template_author: agspades
+year of code: 2026
+template version: 0.1.2
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+inline void solve()
+{
+    int n;
+    cin >> n;
+    vi ar(n);
+    FOR(i, 0, n)
+    cin >> ar[i];
+
+    int mn = *min_element(ar.begin(), ar.end());
+    int mx = *max_element(ar.begin(), ar.end());
+    cout << (mx - mn) + 1 << '\n';
+}
+
+int main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
