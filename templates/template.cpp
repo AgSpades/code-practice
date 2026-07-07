5c33314312bb9c68d501c6e4aca5b558c594004c
Saumyajit P. <mail@saumyajit.dev>
2026-07-07 10:28:34 +0530

    Update template version to 0.1.3 and add and enhance indexed set documentation

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,10 +2,12 @@
 Jai Bajrangbali!
 template_author: agspades
 year of code: 2026
-template version: 0.1.2
+template version: 0.1.3
 */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio               \
     ios::sync_with_stdio(0); \
@@ -20,22 +22,18 @@ using namespace std;
 template <typename T>
 using min_heap = priority_queue<T, vector<T>, greater<T>>;
 
+/*
+Indexed Set
+A set which can be indexed. Methods include:
+- find_by_order(int pos) : returns an iterator to element at given position. note: *iterator give the element.
+- order_of_key(int el): returns the position of a given element in set, element does not appear in the set, we get the position that the element would have in the set.
+*/
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag,
+                         tree_order_statistics_node_update>;
+
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
-
-// Binary Exponentian Function
-ll binpow(ll a, ll b, ll mod = MOD)
-{
-    ll res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = (res * a) % mod;
-        a = (a * a) % mod;
-        b >>= 1;
-    }
-    return res;
-}
 
 inline void solve()
 {
