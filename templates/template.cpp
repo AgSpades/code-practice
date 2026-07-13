eabcaaad25dc64259db2efc4fe30fa3c5d2013b6
Saumyajit P. <mail@saumyajit.dev>
2026-07-13 22:15:45 +0530

    Update .gitignore and template version; add main workspace file

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,18 +2,19 @@
 Jai Bajrangbali!
 template_author: agspades
 year of code: 2026
-template version: 0.1.3
+template version: 0.1.4
 */
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
 
 #define fastio               \
     ios::sync_with_stdio(0); \
     cin.tie(0);              \
     cout.tie(0);
-#define ll long long
+#define int long long
 #define pb push_back
 #define vi vector<int>
 #define vvi vector<vector<int>>
@@ -22,24 +23,24 @@ using namespace __gnu_pbds;
 template <typename T>
 using min_heap = priority_queue<T, vector<T>, greater<T>>;
 
-/*
-Indexed Set
-A set which can be indexed. Methods include:
-- find_by_order(int pos) : returns an iterator to element at given position. note: *iterator give the element.
-- order_of_key(int el): returns the position of a given element in set, element does not appear in the set, we get the position that the element would have in the set.
-*/
-template <typename T>
-using indexed_set = tree<T, null_type, less<T>, rb_tree_tag,
-                         tree_order_statistics_node_update>;
+// /*
+// Indexed Set
+// A set which can be indexed. Methods include:
+// - find_by_order(int pos) : returns an iterator to element at given position. note: *iterator give the element.
+// - order_of_key(int el): returns the position of a given element in set, element does not appear in the set, we get the position that the element would have in the set.
+// */
+// template <typename T>
+// using indexed_set = tree<T, null_type, less<T>, rb_tree_tag,
+//                          tree_order_statistics_node_update>;
 
-const ll MOD = 1e9 + 7;
-const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
 
 inline void solve()
 {
 }
 
-int main()
+int32_t main()
 {
     fastio;
     int t = 1;
---
+++ workspace/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int32_t main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Welcome to AgSpades workspace!" << '\n';
+    }
+    return 0;
+}
